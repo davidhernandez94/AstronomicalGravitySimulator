3d76beaa3b2e2a70943b75e410638b38e9987670
David Hernandez <6309110@edu.vaniercollege.qc.ca>
2025-11-05 15:50:37 -0500

    started planet class

---
+++ src/main/java/com/mycompany/mavenproject2/Planet.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject2;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+/**
+ *
+ * @author David Hernandez
+ */
+public class Planet extends Circle {
+    private DoubleProperty x;
+    private DoubleProperty y;
+    private DoubleProperty radius;
+    private Color colour;
+
+    
+    
+}
