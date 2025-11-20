f0e725322ec62b51a47894fdc7e414db8a049f64
2466920 <2466920@host.docker.internal>
2025-11-20 12:50:51 -0500

    finished the slidebar, used setLayoutX instead of setWidth so that it can appear. Made secondary windows for new planet and satellite

--- src/main/java/com/mycompany/mavenproject2/MainFXMLController.java
+++ src/main/java/com/mycompany/mavenproject2/MainFXMLController.java
@@ -9,13 +9,22 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
  *
- * @author 6324151
+ * @author Astghik Minasyan
  */
 public class MainFXMLController implements Initializable {
 
@@ -25,6 +34,22 @@ public class MainFXMLController implements Initializable {
     private VBox sidebar;
     @FXML
     private Button boton;
+    @FXML
+    private Button newPlanetButton;
+    @FXML
+    private Button newSatelliteButton;
+    @FXML
+    private Button newButton;
+    @FXML
+    private Slider speedSlider;
+    @FXML
+    private Button resetSatellitesButton;
+    @FXML
+    private Button resetAllButton;
+    @FXML
+    private Button settingsButton;
+    @FXML
+    private Pane mainPane;
 
     /**
      * Initializes the controller class.
@@ -38,11 +63,59 @@ public class MainFXMLController implements Initializable {
     private void collapseHandle(ActionEvent event) {
         if (collapseButton.getText().equals(">")) {
             collapseButton.setText("<");
-            sidebar.setPrefWidth(100);
+            sidebar.setTranslateX(0);
         } else {
             collapseButton.setText(">");
-            sidebar.setPrefWidth(0);
+            sidebar.setTranslateX(-180);
         }
     }
+
+    @FXML
+    private void handleNewPlanet(ActionEvent event) {
+        secondWindow("Planet");
+    }
+
+    @FXML
+    private void handleNewSatellite(ActionEvent event) {
+        secondWindow("Satellite");
+    }
+
+    @FXML
+    private void handeLaunch(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleResetSatellites(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleResetAll(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleSettings(ActionEvent event) {
+    }
     
+    public void secondWindow (String name) {
+        Stage secondaryStage = new Stage();
+        secondaryStage.setTitle("New " + name);
+        
+        Label sizeLabel = new Label("Size");
+        Label colorLabel = new Label("Color");
+        Slider sizeSlider = new Slider(0, 30, 0);
+        ColorPicker colorPicker = new ColorPicker(Color.RED); 
+        Button okButton = new Button("Ok");
+        
+        VBox layout = new VBox();
+        layout.getChildren().addAll(sizeLabel, sizeSlider, colorLabel, colorPicker, okButton);
+        layout.setPadding(new Insets(5, 5, 5, 5));
+        layout.setSpacing(15);
+        layout.setAlignment(Pos.CENTER);
+        
+        Scene secondaryScene = new Scene(layout, 500, 300);
+        secondaryStage.setScene(secondaryScene);
+        secondaryStage.show();
+        
+        okButton.setOnAction(e -> secondaryStage.close());
+    }
 }
