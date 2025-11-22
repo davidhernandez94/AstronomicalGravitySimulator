7cf3a3f037ac3fdcb66563723437eb6fa4189067
Astghik-hub <minastghik@gmail.com>
2025-11-22 14:16:22 -0500

    disabled the slider at the beginning, enabled it once the lauch button is clicked, created a listener for speedSlider and for sizeSlider, created addPlanet and addSatellite methods for when the user wants to create a new planet or satellite

--- src/main/java/com/mycompany/mavenproject2/MainFXMLController.java
+++ src/main/java/com/mycompany/mavenproject2/MainFXMLController.java
@@ -6,6 +6,8 @@ package com.mycompany.mavenproject2;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -16,6 +18,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -27,7 +31,10 @@ import javafx.stage.Stage;
  * @author Astghik Minasyan
  */
 public class MainFXMLController implements Initializable {
-    
+
+    public double simulationSpeed;
+    public DoubleProperty radius;
+
     @FXML
     private Button collapseButton;
     @FXML
@@ -56,8 +63,9 @@ public class MainFXMLController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        speedSlider.setDisable(true);
+        simulationSpeed = 0;
+    }
 
     @FXML
     private void collapseHandle(ActionEvent event) {
@@ -91,31 +99,77 @@ public class MainFXMLController implements Initializable {
     @FXML
     private void handleSettings(ActionEvent event) {
     }
-    
+
     @FXML
     private void handleLaunch(ActionEvent event) {
+        simulationSpeed = 1;
+        speedSlider.setDisable(false);
+        simulationSpeedHandler();
     }
-    
-    public void secondWindow (String name) {
+
+    public void secondWindow(String name) {
         Stage secondaryStage = new Stage();
         secondaryStage.setTitle("New " + name);
-        
+
         Label sizeLabel = new Label("Size");
         Label colorLabel = new Label("Color");
+        Label xLabel = new Label("Layout x: ");
+        Label yLabel = new Label("Layout y: ");
         Slider sizeSlider = new Slider(0, 30, 0);
-        ColorPicker colorPicker = new ColorPicker(Color.RED); 
-        Button okButton = new Button("Ok");
-        
+        ColorPicker colorPicker = new ColorPicker(Color.RED);
+        TextField xfield = new TextField();
+        TextField yfield = new TextField();
+
+        HBox coordinateBox = new HBox(xLabel, xfield, yLabel, yfield);
+        coordinateBox.setSpacing(10);
+        coordinateBox.setAlignment(Pos.CENTER);
+
+        Button doneButton = new Button("Done");
+
         VBox layout = new VBox();
-        layout.getChildren().addAll(sizeLabel, sizeSlider, colorLabel, colorPicker, okButton);
+        layout.getChildren().addAll(sizeLabel, sizeSlider, colorLabel, colorPicker, coordinateBox, doneButton);
         layout.setPadding(new Insets(5, 5, 5, 5));
         layout.setSpacing(15);
         layout.setAlignment(Pos.CENTER);
-        
+
         Scene secondaryScene = new Scene(layout, 500, 300);
         secondaryStage.setScene(secondaryScene);
         secondaryStage.show();
-        
-        okButton.setOnAction(e -> secondaryStage.close());
+
+        DoubleProperty xProperty = new SimpleDoubleProperty(Double.parseDouble(xfield.getText()));
+        DoubleProperty yProperty = new SimpleDoubleProperty(Double.parseDouble(yfield.getText()));
+
+        sizeSlider.valueProperty().addListener(cl -> {
+            radius = new SimpleDoubleProperty(sizeSlider.getValue());
+        });
+
+        Color color = colorPicker.getValue();
+
+        doneButton.setOnAction(e -> {
+            secondaryStage.close();
+            
+            if (name.equals("Planet")) {
+                addPlanet(xProperty, yProperty, radius, color);
+            } else {
+                addSatellite(xProperty, yProperty, radius, color);
+            }
+        });
+    }
+    
+    public void simulationSpeedHandler() {
+         speedSlider.valueProperty().addListener(cl -> {
+            simulationSpeed = speedSlider.getValue();
+        });
+    }
+
+    public void addPlanet(DoubleProperty x, DoubleProperty y, DoubleProperty radius, Color color) {
+//        // uncomment once branches merge
+//        Planet planet = new Planet(x, y, radius, color);
+//        mainPane.getChildren().remove(sidebar);
+//        mainPane.getChildren().addAll(planet, sidebar);
+    }
+
+    public void addSatellite(DoubleProperty x, DoubleProperty y, DoubleProperty radius, Color color) {
+        // TODO
     }
 }
