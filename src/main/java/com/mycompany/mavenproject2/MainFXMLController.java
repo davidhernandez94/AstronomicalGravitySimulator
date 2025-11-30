206110b15d148e4e674ffd4752e04191ee941a64
User <6309110@edu.vaniercollege.qc.ca>
2025-11-30 13:12:10 -0500

    started documentation, made alert for satellite overlaps, improved speed slider

--- src/main/java/com/mycompany/mavenproject2/MainFXMLController.java
+++ src/main/java/com/mycompany/mavenproject2/MainFXMLController.java
@@ -9,8 +9,6 @@ import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.animation.PauseTransition;
 import javafx.animation.TranslateTransition;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -39,14 +37,12 @@ import javafx.util.Duration;
  */
 public class MainFXMLController implements Initializable {
 
-    private Duration simulationSpeed;
-    private TranslateTransition transition = new TranslateTransition();
-    private boolean running = false;
-    private ArrayList<Planet> planets = new ArrayList<>();
-    private PauseTransition pauseTrans = new PauseTransition(new Duration(1000));
-
-    // should be removed after, see IMPORTANT comment way below
-    private Satellite satellite;
+    private Duration simulationSpeed; // duration of each individual transition of satellite
+    private TranslateTransition transition = new TranslateTransition(); // linear transition of satellite 
+    private boolean running = false; // true when animaiton is running
+    private ArrayList<Planet> planets = new ArrayList<>(); // list of planets currently in animation
+    private PauseTransition pauseTrans = new PauseTransition(new Duration(1000)); // 1-second transition to show crash label
+    private Satellite satellite; // satellite of animation
 
     @FXML
     private Button collapseButton;
@@ -63,8 +59,6 @@ public class MainFXMLController implements Initializable {
     @FXML
     private Button resetAllButton;
     @FXML
-    private Button settingsButton;
-    @FXML
     private StackPane simPane;
     @FXML
     private Button lauchButton;
@@ -75,17 +69,20 @@ public class MainFXMLController implements Initializable {
 
     /**
      * Initializes the controller class.
+     * Setting up default configurations
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         crashLabel.setVisible(false);
         lauchButton.setDisable(true);
         speedSlider.setDisable(true);
-        speedSlider.setMin(.5);
-        speedSlider.setMax(6);
         resetAllButton.setDisable(true);
-        simulationSpeed = new Duration(50);
-        pauseTrans.setOnFinished(eh -> crashLabel.setVisible(false));
+        
+        // hides crash label once 1 second is over
+        pauseTrans.setOnFinished(eh -> crashLabel.setVisible(false)); 
+        
+        // when transition is finished, run it again so that it's continuous 
+        // and there's smooth motion
         transition.setOnFinished(eh -> {
             if (running) {
                 launch();
@@ -95,97 +92,104 @@ public class MainFXMLController implements Initializable {
         handleResetAll();
     }
 
+    /**
+     * collapse sidebar in main scene
+     * @param event triggered action event
+     */
     @FXML
     private void collapseHandle(ActionEvent event) {
+        // moves sidebar outside/inside of scene by translation
         if (collapseButton.getText().equals(">")) {
             collapseButton.setText("<");
             sidebar.setTranslateX(0);
+            
         } else {
             collapseButton.setText(">");
             sidebar.setTranslateX(-180);
         }
     }
 
+    /**
+     * when newPlanetButton is clicked, open second window
+     * @param event triggered action event
+     */
     @FXML
     private void handleNewPlanet(ActionEvent event) {
         secondWindow("Planet");
     }
 
+    /**
+     * when newSatelliteButton is clicked, open second window
+     * @param event triggered action event 
+     */
     @FXML
     private void handleNewSatellite(ActionEvent event) {
         secondWindow("Satellite");
     }
 
+    /**
+     * when resetSatelliteButton is clicked, restart animation from beginning
+     * satellite fields all return to default values
+     * @param event triggered action event
+     */
     @FXML
     private void handleResetSatellite(ActionEvent event) {
         if (satellite == null) {
             return;
         }
 
+        // stop animation
         transition.stop();
         running = false;
 
+        // reset satellite fields
         satellite.velX = 0;
         satellite.velY = 0;
         satellite.accX = 0;
         satellite.accY = 0;
-
         satellite.posX = satellite.initialX;
         satellite.posY = satellite.initialY;
 
+        // bring satellite to original position
         satellite.circle.setTranslateX(satellite.initialX);
         satellite.circle.setTranslateY(satellite.initialY);
 
         lauchButton.setDisable(false);
     }
 
+    /**
+     * reset the whole scene
+     */
     @FXML
     private void handleResetAll() {
+        // remove satellite from simPane
         if (satellite != null) {
             if (satellite.circle != null) {
                 simPane.getChildren().remove(satellite.circle);
             }
+            
             satellite = null;
             newSatelliteButton.setDisable(false);
             lauchButton.setDisable(true);
             running = false;
         }
 
+        // remove all planets from simPane
         if (!planets.isEmpty()) {
             for (Planet planet : planets) {
                 if (planet != null && planet.circle != null) {
                     simPane.getChildren().remove(planet.circle);
                 }
             }
+            
             planets.clear();
         }
     }
 
-    @FXML
-    private void handleSettings(ActionEvent event) {
-        Stage settingsStage = new Stage();
-        settingsStage.setTitle("Settings");
-
-        Label volumeLabel = new Label("Volume: ");
-        Slider volumeSlider = new Slider(0, 100, 100);
-        volumeSlider.setShowTickLabels(true);
-        volumeSlider.setShowTickMarks(true);
-        volumeSlider.setMajorTickUnit(20);
-
-        HBox volumeBox = new HBox(volumeLabel, volumeSlider);
-
-        volumeBox.setPadding(new Insets(5, 5, 5, 5));
-        volumeBox.setSpacing(15);
-        volumeBox.setAlignment(Pos.CENTER);
-
-        volumeSlider.valueProperty().addListener(new ChangeListener<Number>() {
-            @Override
-            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                // TODO: sound
-            }
-        });
-    }
-
+    /**
+     * when launch button is clicked, call animation 
+     * @param event triggered action event
+     */
     @FXML
     private void handleLaunch(ActionEvent event) {
         speedSlider.setDisable(false);
@@ -193,10 +197,18 @@ public class MainFXMLController implements Initializable {
         launch();
     }
 
+    /**
+     * second window: outside of first window, so MainFXML does not apply here.
+     * lets user create a new planet or satellite
+     * choices: colour, x-position, y-position, and size (only for planets)
+     * @param name either "Satellite" or "Planet"
+     */
     public void secondWindow(String name) {
+        // initialize new window
         Stage secondaryStage = new Stage();
         secondaryStage.setTitle("New " + name);
 
+        // manually creating labels and nodes to put in scene
         Label sizeLabel = new Label("Size");
         Label colorLabel = new Label("Color");
         Label xLabel = new Label("Layout x: ");
@@ -206,49 +218,62 @@ public class MainFXMLController implements Initializable {
         TextField xfield = new TextField();
         TextField yfield = new TextField();
 
+        // settings for size slider
         sizeSlider.setMajorTickUnit(10);
         sizeSlider.setShowTickMarks(true);
         sizeSlider.setShowTickLabels(true);
 
+        // satellite size is invariable, so disable the size slider
         if (name.equals("Satellite")) {
             sizeSlider.setDisable(true);
         }
 
+        // setting for Hbox
         HBox coordinateBox = new HBox(xLabel, xfield, yLabel, yfield);
         coordinateBox.setSpacing(10);
         coordinateBox.setAlignment(Pos.CENTER);
 
+        // done button 
         Button doneButton = new Button("Done");
         doneButton.setDisable(true);
 
+        // settings for vbox
         VBox layout = new VBox();
         layout.getChildren().addAll(sizeLabel, sizeSlider, colorLabel, colorPicker, coordinateBox, doneButton);
         layout.setPadding(new Insets(5, 5, 5, 5));
         layout.setSpacing(15);
         layout.setAlignment(Pos.CENTER);
 
+        // initialize scene 
         Scene secondaryScene = new Scene(layout, 500, 300);
         secondaryStage.setResizable(false);
         secondaryStage.setScene(secondaryScene);
         secondaryStage.show();
 
+        // handler that checks to see if values are integers
+        // note: planets can be placed outside of scene
         EventHandler coordinateHandler = (EventHandler<KeyEvent>) ((KeyEvent) -> {
-            if (xfield.getText().matches("^\\d+$") && yfield.getText().matches("^\\d+$")) {
+            if (xfield.getText().matches("^-?\\d+$") && yfield.getText().matches("^-?\\d+$")) {
                 doneButton.setDisable(false);
             } else {
                 doneButton.setDisable(true);
             }
         });
 
+        // since a simple double value can't be changed within a lambda expresssion, 
+        // make it an array with only one element. 
+        // links radius of planet with size slider
         double[] radius = new double[1];
         radius[0] = sizeSlider.getValue();
         sizeSlider.valueProperty().addListener(cl -> {
             radius[0] = sizeSlider.getValue();
         });
 
+        // check to see if x and y values are valid
         xfield.setOnKeyReleased(coordinateHandler);
         yfield.setOnKeyReleased(coordinateHandler);
 
+        // when done, make new planet/satellite and add to simPane
         doneButton.setOnAction(e -> {
             double xProperty = Double.parseDouble(xfield.getText());
             double yProperty = Double.parseDouble(yfield.getText());
@@ -267,10 +292,12 @@ public class MainFXMLController implements Initializable {
         });
     }
 
+    /**
+     * update simulationSpeed when slider is changed
+     */
     @FXML
     public void simulationSpeedHandler() {
-        System.out.println("speed: " + speedSlider.getValue());
-        simulationSpeed = new Duration((int) (60 / speedSlider.getValue()));
+        simulationSpeed = new Duration((int) (100 / speedSlider.getValue()));
     }
 
     public void addPlanet(double x, double y, double radius, Color color) {
@@ -302,7 +329,7 @@ public class MainFXMLController implements Initializable {
     public void launch() {
         // main simulation is here
 
-        transition.setDuration(simulationSpeed.divide(20));
+        transition.setDuration(simulationSpeed);
 
         running = true;
         double forceX = 0;
@@ -326,8 +353,8 @@ public class MainFXMLController implements Initializable {
             }
         }
         satellite.circle.toFront();
-        satellite.accX = forceX / 8_000_000;
-        satellite.accY = forceY / 8_000_000;
+        satellite.accX = forceX / 80_000;
+        satellite.accY = forceY / 80_000;
         satellite.changeVelocity();
         satellite.changePosition();
         transition.setNode(satellite.circle);
